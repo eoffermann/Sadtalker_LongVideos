5abea53f95a890dfab3e6f5b2689cbb8ffa289a6
Eddie Offermann <eddie@bigblueceiling.com>
2024-02-16 13:34:22 -0800

    Moving the active inference pipeline from main to perform_inference - this more modular approach improves the ability to integrate this module into larger projects. We use Namespace from argparse to encapsulate arguments sent to perform_inference since some downstream modules also expect that the overall project was launched from the commandline. (TODO: remove this weird presumption downstream and pass necessary values explicitly instead of passing a Namespace object.)

--- inference.py
+++ inference.py
@@ -33,6 +33,7 @@ import shutil
 import os, sys
 from time import  strftime
 from argparse import ArgumentParser
+from argparse import Namespace
 from pathlib import Path
 
 import torch
@@ -56,34 +57,75 @@ def folder_remove(folder):
 		except Exception as e:
 			print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-def main(args):
-    #torch.backends.cudnn.enabled = False
-    pic_path = args.source_image
-    audio_path = args.driven_audio
-    pose_style = args.pose_style
-    device = args.device
-    batch_size = args.batch_size
-    input_yaw_list = args.input_yaw
-    input_pitch_list = args.input_pitch
-    input_roll_list = args.input_roll
-    ref_eyeblink = args.ref_eyeblink
-    ref_pose = args.ref_pose
-    result_dir = args.result_dir
-    checkpoint_dir = args.checkpoint_dir
-    size = args.size
-    old_version = args.old_version
-    preprocess = args.preprocess
-    still = args.still
-    face3dvis = args.face3dvis
-    expression_scale = args.expression_scale
-    preprocess = args.preprocess
-    size = args.size
-    enhancer = args.enhancer
-    background_enhancer = args.background_enhancer
-
+def perform_inference(
+    source_image='./examples/source_image/00035-3227700327.png',  # Path to source image
+    driven_audio='./examples/driven_audio/imagine.wav',  # Path to driven audio
+    pose_style=0,  # Input pose style from [0, 46)
+    device='cuda',  # Use cuda for inference by default. Supported values are 'cuda' or 'cpu'
+    batch_size=2,  # The batch size of facerender
+    input_yaw_list=None,  # The input yaw degree of the user, default is None indicating no specific yaw
+    input_pitch_list=None,  # The input pitch degree of the user, default is None indicating no specific pitch
+    input_roll_list=None,  # The input roll degree of the user, default is None indicating no specific roll
+    ref_eyeblink=None,  # Path to reference video providing eye blinking
+    ref_pose=None,  # Path to reference video providing pose
+    result_dir='./results',  # Path to output directory for results
+    checkpoint_dir='./checkpoints',  # Path to checkpoints directory
+    size=512,  # The image size of the facerender
+    old_version=False,  # Use the pth other than safetensor version, indicates older model version usage
+    preprocess='extfull',  # How to preprocess the images, 'extfull' by default
+    still=False,  # Can crop back to the original videos for the full body animation
+    face3dvis=False,  # Generate 3d face and 3d landmarks visualization
+    expression_scale=1.0,  # Scale of expression adjustments
+    enhancer="gfpgan",  # Face enhancer, options include "gfpgan"
+    background_enhancer=None,  # Background enhancer, default is None indicating no enhancement
+    verbose=False,  # Saving the intermediate output or not
+    net_recon='resnet50',  # Network architecture for reconstruction, 'resnet50' by default
+    use_last_fc=False,  # Zero initialize the last fc layer
+    bfm_folder='./checkpoints/BFM_Fitting/',  # Path to BFM fitting folder
+    bfm_model='BFM_model_front.mat',  # BFM model file name
+    focal=1015.,  # Focal length for renderer
+    center=112.,  # Center for renderer
+    camera_d=10.,  # Camera distance for renderer
+    z_near=5.,  # Near plane for renderer
+    z_far=15.  # Far plane for renderer
+    ):
+    
+    # Create an args object to encapsulate the inputs and preserve compatibility downstream
+    args = Namespace(
+        source_image=source_image,
+        driven_audio=driven_audio,
+        pose_style=pose_style,
+        device=device,
+        batch_size=batch_size,
+        input_yaw=input_yaw_list,
+        input_pitch=input_pitch_list,
+        input_roll=input_roll_list,
+        ref_eyeblink=ref_eyeblink,
+        ref_pose=ref_pose,
+        result_dir=result_dir,
+        checkpoint_dir=checkpoint_dir,
+        size=size,
+        old_version=old_version,
+        preprocess=preprocess,
+        still=still,
+        face3dvis=face3dvis,
+        expression_scale=expression_scale,
+        enhancer=enhancer,
+        background_enhancer=background_enhancer,
+        verbose=verbose,
+        net_recon=net_recon,
+        use_last_fc=use_last_fc,
+        bfm_folder=bfm_folder,
+        bfm_model=bfm_model,
+        focal=focal,
+        center=center,
+        camera_d=camera_d,
+        z_near=z_near,
+        z_far=z_far
+    )
 
     save_dir = os.path.join(result_dir, strftime("%Y_%m_%d_%H.%M.%S"))
-    aud_name = Path(audio_path).stem
+    aud_name = Path(driven_audio).stem
     aud_folder = Path(save_dir)
     
     save_dir = aud_folder/'temp'
@@ -110,7 +152,7 @@ def main(args):
     os.makedirs(first_frame_dir, exist_ok=True)
     print('3DMM Extraction for source image')
     
-    first_coeff_path, crop_pic_path, crop_info =  preprocess_model.generate(pic_path, first_frame_dir, preprocess, source_image_flag=True, pic_size=size)
+    first_coeff_path, crop_source_image, crop_info =  preprocess_model.generate(source_image, first_frame_dir, preprocess, source_image_flag=True, pic_size=size)
     
     if first_coeff_path is None:
         print("Can't get the coeffs of the input")
@@ -138,56 +180,91 @@ def main(args):
         ref_pose_coeff_path=None
 
     #audio2ceoff
-    batch = get_data(first_coeff_path, audio_path, device, ref_eyeblink_coeff_path, still=still)
+    batch = get_data(first_coeff_path, driven_audio, device, ref_eyeblink_coeff_path, still=still)
     coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
 
     # 3dface render
     if face3dvis:
         from src.face3d.visualize import gen_composed_video
-        gen_composed_video(args, device, first_coeff_path, coeff_path, audio_path, os.path.join(save_dir, '3dface.mp4'))
+        gen_composed_video(args, device, first_coeff_path, coeff_path, driven_audio, os.path.join(save_dir, '3dface.mp4'))
     
     #coeff2video
-    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, 
+    data = get_facerender_data(coeff_path, crop_source_image, first_coeff_path, driven_audio, 
                                 batch_size, input_yaw_list, input_pitch_list, input_roll_list,
                                 expression_scale=expression_scale, still_mode=still, preprocess=preprocess, size=size)
     
-    result, temp_dir = animate_from_coeff.generate(args, data, save_dir, pic_path, crop_info, enhancer=enhancer, background_enhancer=background_enhancer, preprocess=preprocess, img_size=size)
+    result, temp_dir = animate_from_coeff.generate(args, data, save_dir, source_image, crop_info, enhancer=enhancer, background_enhancer=background_enhancer, preprocess=preprocess, img_size=size)
     shutil.move(result, save_fn)
     print('The generated video is named:', save_fn)
     folder_remove(temp_dir)
 
-    # if not args.verbose:
-    #     shutil.rmtree(save_dir)
+def main(args):
+    perform_inference(
+        source_image=args.source_image,
+        driven_audio=args.driven_audio,
+        pose_style=args.pose_style,
+        device='cuda' if not args.cpu else 'cpu',  # Adjusted for --cpu flag
+        batch_size=args.batch_size,
+        input_yaw_list=args.input_yaw,
+        input_pitch_list=args.input_pitch,
+        input_roll_list=args.input_roll,
+        ref_eyeblink=args.ref_eyeblink,
+        ref_pose=args.ref_pose,
+        result_dir=args.result_dir,
+        checkpoint_dir=args.checkpoint_dir,
+        size=args.size,
+        old_version=args.old_version,
+        preprocess=args.preprocess,
+        still=args.still,
+        face3dvis=args.face3dvis,
+        expression_scale=args.expression_scale,
+        enhancer=args.enhancer,
+        background_enhancer=args.background_enhancer,
+        verbose=args.verbose, 
+        net_recon=args.net_recon,
+        use_last_fc=args.use_last_fc, 
+        bfm_folder=args.bfm_folder,  
+        bfm_model=args.bfm_model, 
+        focal=args.focal,  
+        center=args.center, 
+        camera_d=args.camera_d, 
+        z_near=args.z_near, 
+        z_far=args.z_far
+    )
+
 
     
 if __name__ == '__main__':
     parser = ArgumentParser()  
-    parser.add_argument("--driven_audio", default='./examples/driven_audio/imagine.wav', help="path to driven audio")
     parser.add_argument("--source_image", default='./examples/source_image/00035-3227700327.png', help="path to source image")
-    parser.add_argument("--ref_eyeblink", default=None, help="path to reference video providing eye blinking")
-    parser.add_argument("--ref_pose", default=None, help="path to reference video providing pose")
-    parser.add_argument("--checkpoint_dir", default='./checkpoints', help="path to output")
-    parser.add_argument("--result_dir", default='./results', help="path to output")
+    parser.add_argument("--driven_audio", default='./examples/driven_audio/imagine.wav', help="path to driven audio")
     parser.add_argument("--pose_style", type=int, default=0,  help="input pose style from [0, 46)")
+    # device would be defined here
     parser.add_argument("--batch_size", type=int, default=2,  help="the batch size of facerender")
-    parser.add_argument("--size", type=int, default=512,  help="the image size of the facerender")
-    parser.add_argument("--expression_scale", type=float, default=1.,  help="the batch size of facerender")
+
     parser.add_argument('--input_yaw', nargs='+', type=int, default=None, help="the input yaw degree of the user ")
     parser.add_argument('--input_pitch', nargs='+', type=int, default=None, help="the input pitch degree of the user")
     parser.add_argument('--input_roll', nargs='+', type=int, default=None, help="the input roll degree of the user")
+    parser.add_argument("--ref_eyeblink", default=None, help="path to reference video providing eye blinking")
+    parser.add_argument("--ref_pose", default=None, help="path to reference video providing pose")
+
+    parser.add_argument("--result_dir", default='./results', help="path to output")
+    parser.add_argument("--checkpoint_dir", default='./checkpoints', help="path to output")
+    parser.add_argument("--size", type=int, default=512,  help="the image size of the facerender")
+    parser.add_argument("--old_version",action="store_true", help="use the pth other than safetensor version" ) 
+    parser.add_argument("--preprocess", default='extfull', choices=['crop', 'extcrop', 'resize', 'full', 'extfull'], help="how to preprocess the images" ) 
+
+    parser.add_argument("--still", action="store_true", help="can crop back to the original videos for the full body aniamtion") 
+    parser.add_argument("--face3dvis", action="store_true", help="generate 3d face and 3d landmarks") 
+    parser.add_argument("--expression_scale", type=float, default=1.,  help="the batch size of facerender")
     parser.add_argument('--enhancer',  type=str, default="gfpgan", help="Face enhancer, [gfpgan, RestoreFormer]")
     parser.add_argument('--background_enhancer',  type=str, default=None, help="background enhancer, [realesrgan]")
-    parser.add_argument("--cpu", dest="cpu", action="store_true") 
-    parser.add_argument("--face3dvis", action="store_true", help="generate 3d face and 3d landmarks") 
-    parser.add_argument("--still", action="store_true", help="can crop back to the original videos for the full body aniamtion") 
-    parser.add_argument("--preprocess", default='extfull', choices=['crop', 'extcrop', 'resize', 'full', 'extfull'], help="how to preprocess the images" ) 
-    parser.add_argument("--verbose",action="store_true", help="saving the intermedia output or not" ) 
-    parser.add_argument("--old_version",action="store_true", help="use the pth other than safetensor version" ) 
 
+    parser.add_argument("--cpu", dest="cpu", action="store_true") 
+    parser.add_argument("--verbose",action="store_true", help="saving the intermedia output or not" ) 
 
     # net structure and parameters
     parser.add_argument('--net_recon', type=str, default='resnet50', choices=['resnet18', 'resnet34', 'resnet50'], help='useless')
-    parser.add_argument('--init_path', type=str, default=None, help='Useless')
     parser.add_argument('--use_last_fc',default=False, help='zero initialize the last fc')
     parser.add_argument('--bfm_folder', type=str, default='./checkpoints/BFM_Fitting/')
     parser.add_argument('--bfm_model', type=str, default='BFM_model_front.mat', help='bfm model')
